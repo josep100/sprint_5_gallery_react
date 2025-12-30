98740d9438ff5cad6a4d2556688812e8d437598d
josep <josep_1000@hotmail.com>
2025-12-31 00:16:48 +0100

    feat: implement drag-and-drop reordering

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,24 +1,50 @@
 import { useState } from "react";
+import { DndContext, closestCenter,  } from '@dnd-kit/core';
+import type { DragEndEvent } from '@dnd-kit/core';
+import { SortableContext, arrayMove } from '@dnd-kit/sortable';
 import photos from "@/data/dataPhoto";
 import ImageItem from "../ImageItem/ImageItem";
 
+
 const Gallery = () => {
-    const [gallery, setGallery] = useState(photos);
+  const [gallery, setGallery] = useState(photos);
+  
+  const handleDelete = (id: string) => {
+    if (window.confirm("¿Eliminar esta imagen?")) {
+      setGallery(prev => prev.filter(img => img.id !== id));
+    }
+  };
 
-    const handleDelete = (id: string) => {
-        if (window.confirm('¿Eliminar esta imagen?')) {
-            setGallery(gallery.filter(img => img.id !== id));
-        }
-    };
-
-    return (
-        <section className="container mx-auto mt-7 min-h-212.5 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 p-0">
-            {gallery.map((photo, indice) => (
-                <ImageItem url = {photo.download_url} author = {photo.author} isFeatured = {indice === 0}  key = {photo.id} onDelete = {handleDelete} id = {photo.id } /> 
-            ))}
+  const handleDragEnd = (event: DragEndEvent) => {
+      const {active, over} = event;
+      if (active.id !== over?.id) {
+          setGallery((copy = [...gallery]) => {
+            const oldIndex = copy.findIndex(item => item.id === active.id);
+            const newIndex = copy.findIndex(item => item.id === over?.id);
             
-        </section>
-    );
+            return arrayMove(copy, oldIndex, newIndex);
+          });
+      }
+  };
+
+  return (
+      <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+          <SortableContext items={gallery.map(photo => photo.id)}>
+              <section className="container mx-auto mt-7 min-h-212.5 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
+                {gallery.map((photo, index) => (
+                    <ImageItem
+                      key={photo.id}
+                      id={photo.id}
+                      url={photo.download_url}
+                      author={photo.author}
+                      isFeatured={index === 0}
+                      onDelete={handleDelete}
+                    />
+                ))}
+              </section>
+          </SortableContext>
+      </DndContext>
+  );
 };
 
 export default Gallery;
--- src/components/ImageItem/ImageItem.tsx
+++ src/components/ImageItem/ImageItem.tsx
@@ -1,31 +1,48 @@
 import type { ImageItemProps } from "@/types/photo";
-import { Trash2 } from 'lucide-react';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card"
+import { Trash2 } from "lucide-react";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
-const ImageItem = ({url, author, isFeatured, onDelete, id}: ImageItemProps) => { 
-        
-    return (
-            <Card className={isFeatured ? "col-span-2 row-span-3" : ""}>
-                <CardContent>
-                        <figure>
-                            <img src={url} alt={author} />
-                        </figure> 
-                </CardContent>
-                <CardFooter className="flex-col gap-2">
-                   <p className="w-full flex justify-around items-center">
-                        {author}
-                         <Trash2 className="cursor-pointer size-7 text-[#ff1a1a] stroke-[2.25px]" onClick = {() => {
-                             onDelete(id);
-                         }} /> 
-                    </p>
-                </CardFooter>
-            </Card>
-    );
+const ImageItem = ({
+  id,
+  url,
+  author,
+  isFeatured,
+  onDelete
+}: ImageItemProps) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({id});
 
-}
+  const style = {
+    transform: CSS.Translate.toString(transform),
+    transition,
+  };
+
+  return (
+    <Card ref={setNodeRef} style={style} {...attributes} className={`${isFeatured ? "col-span-2 row-span-3" : ""} ${isDragging ? "cursor-grabbing col-span-1 row-span-1" : "cursor-grab"}`}>
+      <CardContent>
+        <figure {...listeners} >
+          <img src={url} alt={author} />
+        </figure>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <p className="w-full flex justify-around items-center">
+          {author}
+          <Trash2
+            className="cursor-pointer size-7 text-[#ff1a1a] stroke-[2.25px]"
+            onClick={() => onDelete(id)}
+          />
+        </p>
+      </CardFooter>
+    </Card>
+  );
+};
 
 export default ImageItem;
